8c216887a14a9df0ba8311714dfb2e2e752cf817
Jaxon Adams <jadamsresume1@gmail.com>
2022-05-17 17:42:24 -0600

    service worker hotfix

--- public/service-worker.js
+++ public/service-worker.js
@@ -43,10 +43,10 @@ self.addEventListener('activate', function(e) {
 self.addEventListener('fetch', function(e) {
     console.log(`Fetch request : ${e.request.url}`);
     e.respondWith(
-        caches.match(e.request).then(function(request) {
-            if (request) {
+        caches.match(e.request).then(function(response) {
+            if (response) {
                 console.log('responding with cache : ' + e.request.url)
-                return request
+                return response
             } else {
                 console.log('file is not cached, fetching : ' + e.request.url)
                 return fetch(e.request)
